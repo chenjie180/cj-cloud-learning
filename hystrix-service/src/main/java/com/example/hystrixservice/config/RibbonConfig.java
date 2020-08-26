7e7389fd4d4be991f869c288bd0f480cd3f4af0f
chenj <1037989610@qq.com>
2020-08-26 21:11:05 +0800

    Initial commit

---
+++ hystrix-service/src/main/java/com/example/hystrixservice/config/RibbonConfig.java
@@ -0,0 +1,21 @@
+package com.example.hystrixservice.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author shkstart
+ * @create 2020-08-25 16:25
+ */
+@Configuration
+public class RibbonConfig {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
---
+++ hystrix-service/src/main/java/com/example/hystrixservice/controller/UserHystrixController.java
@@ -0,0 +1,51 @@
+package com.example.hystrixservice.controller;
+
+import com.example.hystrixservice.domain.CommonResult;
+import com.example.hystrixservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shkstart
+ * @create 2020-08-25 17:23
+ */
+@RestController
+public class UserHystrixController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/testFallback/{id}")
+    public CommonResult testFallback(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+    @GetMapping("/testCommand/{id}")
+    public CommonResult testCommand(@PathVariable Long id) {
+        return userService.getUserCommand(id);
+    }
+
+    @GetMapping("/testException/{id}")
+    public CommonResult testException(@PathVariable Long id) {
+        return userService.getUserException(id);
+    }
+
+    @GetMapping("/testCache/{id}")
+    public CommonResult testCache(@PathVariable Long id) {
+        userService.getUserCache(id);
+        userService.getUserCache(id);
+        userService.getUserCache(id);
+        return new CommonResult("操作成功", 200);
+    }
+
+    @GetMapping("/testRemoveCache/{id}")
+    public CommonResult testRemoveCache(@PathVariable Long id) {
+        userService.getUserCache(id);
+        userService.removeCache(id);
+        userService.getUserCache(id);
+        return new CommonResult("操作成功", 200);
+    }
+
+
+}
